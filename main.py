83de26131f3e34d41ee48512a3e852883ecba81d
GenkiHirano <genki86web@gmail.com>
2023-05-16 23:54:30 +0900

    feat: mainファイル実装

---
+++ main.py
@@ -0,0 +1,28 @@
+from langchain import LLMChain
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+import os
+
+os.environ["OPENAI_API_KEY"] = "..."
+
+
+# OpenAIのモデルのインスタンスを作成
+llm = OpenAI(model_name="text-davinci-003")
+
+# プロンプトのテンプレート文章を定義
+template = """
+次の文章に誤字がないか調べて。誤字があれば訂正してください。
+{sentences_before_check}
+"""
+
+# テンプレート文章にあるチェック対象の単語を変数化
+prompt = PromptTemplate(
+    input_variables=["sentences_before_check"],
+    template=template,
+)
+
+# OpenAIのAPIにこのプロンプトを送信するためのチェーンを作成
+chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+
+# チェーンを実行し、結果を表示
+print(chain("こんんちわ、元気です。"))
