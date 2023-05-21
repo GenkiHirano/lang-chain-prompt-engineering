d3f39104b46d8da31258ca2cfb2714f349d6064f
GenkiHirano <genki86web@gmail.com>
2023-05-22 00:02:32 +0900

    feat: Text Embedding Modelsでベクトルを出力

---
+++ llms.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+os.getenv("OPENAI_API_KEY")
+
+from langchain.embeddings import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings()
+query_result = embeddings.embed_query("ITエンジニアについて30文字で教えて。")
+
+# print(query_result)
+print(len(query_result))
