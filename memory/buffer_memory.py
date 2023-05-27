2d474a07f2adcf554db985436058c0b1da94c89b
GenkiHirano <genki86web@gmail.com>
2023-05-27 17:36:18 +0900

    feat: Buffer Memoryの使い方

--- memory/buffer_memory.py
+++ memory/buffer_memory.py
@@ -7,6 +7,17 @@ os.getenv("OPENAI_API_KEY")
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 
+memory = ConversationBufferMemory()
+
+memory.save_context(
+    {"input": "AIとは何？"},
+    {"output": "AIとは、人工知能のことです。"},
+)
+print(memory.load_memory_variables({}))
+
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
+
 memory = ConversationBufferMemory(return_messages=True)
 
 memory.save_context(
