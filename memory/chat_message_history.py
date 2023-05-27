fd100a637241959ab2fda7607bd2d34d34b1dc8b
GenkiHirano <genki86web@gmail.com>
2023-05-27 17:24:44 +0900

    feat: Chat Message Historyの使い方

---
+++ memory/chat_message_history.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+os.getenv("OPENAI_API_KEY")
+
+from langchain.memory import ChatMessageHistory
+from langchain.schema import messages_to_dict
+from langchain.schema import messages_from_dict
+
+history = ChatMessageHistory()
+
+history.add_user_message("AIとは何？")
+history.add_ai_message("AIとは、人工知能のことです。")
+
+message_list = history.messages
+message_dict = messages_to_dict(message_list)
+print(message_list)
+print(message_dict)
+
+message_list_from_dict = messages_from_dict(message_dict)
+print(message_list_from_dict)
